415e6d0a9495f703947d3fe8d058e6957b73ef76
ABDOUNIIAHM <abdellah.abdouni@gmail.com>
2023-05-17 08:53:11 +0200

    created repo, .properties ignored, pom.xml updated

---
+++ src/main/java/com/example/tp_validation_4/TpValidation4Application.java
@@ -0,0 +1,13 @@
+package com.example.tp_validation_4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpValidation4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TpValidation4Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tp_validation_4/TpValidation4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tp_validation_4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpValidation4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
